bb0b1744aefa8a33b881a9933fb378c4962bac59
YmCris <ymcrisystems@gmail.com>
2026-02-25 17:48:13 -0600

    Adding the android project (again)

--- Flowchart App/SourceCode/Android/app/src/main/java/ymcris/compilers/flowchart/backend/domain/flow_chart/Element.kt
+++
@@ -1,8 +0,0 @@
-package ymcris.compilers.flowchart.backend.domain.flow_chart;
-class Element {
-
-    var index: int;
-    var figure: FigureType;
-
-
-}
--- Flowchart App/SourceCode/Android/settings.gradle.kts
+++ Flowchart App/SourceCode/Android/settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "FlowChart"
+rootProject.name = "Flowchart"
 include(":app")
