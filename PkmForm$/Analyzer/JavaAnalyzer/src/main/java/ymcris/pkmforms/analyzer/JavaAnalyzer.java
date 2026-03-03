3aff99b6ff744fb3d0eb4162d2b6cb5a133c9cb9
YmCris <ymcrisystems@gmail.com>
2026-03-03 13:16:57 -0600

    Adding repo project one

---
+++ PkmForm$/Analyzer/JavaAnalyzer/src/main/java/ymcris/pkmforms/analyzer/JavaAnalyzer.java
@@ -0,0 +1,12 @@
+package ymcris.pkmforms.analyzer;
+
+/**
+ *
+ * @author YmCris
+ */
+public class JavaAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
